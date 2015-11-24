691cdaecf5fe21742154bba3504b661d84719f4e
Ryan L Murphy <rmurphy2718@gmail.com>
2015-11-23 22:17:18 -0500

    I added some comments on how to run the file

--- pgm.pl
+++ pgm.pl
@@ -1,1 +1,6 @@
+# Run this perl program!  You can do it in your git bash because 
+# Git bash supports unix commands, and unix has Perl
+# run it like this:
+# perl pgm.pl
+# zhu ni hao yun!
 print("hello, world!\n");
